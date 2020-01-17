3ee5651d948ad25bf11812f534db9f83b56fd179
zakhele.khumalo.ms@gmail.com <zakhele.khumalo.ms@gmail.com>
2020-01-17 15:24:21 +0200

    javascript_image_change
    This Javascript application allow you to change an Image on 'MouseOn' and on 'MouseOut'

---
+++ image.js
@@ -0,0 +1,10 @@
+// change image on Mouse on
+function onMouseOn() {
+	
+	document.getElementById("image").src ="Desert.jpg"
+}
+// change image on Mouse out
+function onMouseOut() {
+	
+	document.getElementById("image").src ="Koala.jpg"
+}
